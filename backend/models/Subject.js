c8ef88a8a05c3004fc00329378c71d7daf25aa2f
Shahul Hameed <146325988+shahulyaalh@users.noreply.github.com>
2025-05-19 14:11:06 +0530

    Update Subject.js

--- backend/models/Subject.js
+++ backend/models/Subject.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 const subjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
   code: { type: String, required: true, unique: false },
   fees: { type: String, required: true },
-  type: { type: String }, // "regular" or "arrear"
-  examSchedule: { type: String }, // e.g., "2025-05-10"
-  semester: { type: String },
-  department: { type: String },
+  
 });
 
 const Subject = mongoose.model("Subject", subjectSchema);
